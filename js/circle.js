b532cb18faefc5e14bd24c24b9d958493c1b5035
t15483hs <t15483hs@sfc.keio.ac.jp>
2016-04-28 10:54:09 +0900

    最も大きい面積を返す
    練習問題3
    面積の変数をa(area)をつけて答えた。
    関数名はMaxAreaでMAとした。

--- js/circle.js
+++ js/circle.js
@@ -7,3 +7,11 @@ function A(a){
     var Pi =3.14159;
     return a*a*Pi;
 }
+
+function MA (a,b,c){
+    var Pi = 3.14159;
+    var aa = a*a*Pi
+    var ba = b*b*Pi
+    var ca = c*c*Pi
+    return Math.max(aa,ba,ca)
+}
